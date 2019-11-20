e6e5bd898cf87c62ec0e51f67f5d5c72423eebe5
sunnyko3 <57108786+sunnyko3@users.noreply.github.com>
2019-11-20 15:41:25 -0700

    react-drills

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Image from "./Image";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Image url={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuYHtwP4IW3z_Jlh-CZApihNOwKWRwN5ODRfapFeS3oRvUrIAj&s"}/>
       </div>
     );
   }
---
+++ app-5/src/Image.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class Image extends Component {
+    render(){
+        return(
+            <div>
+                <img src={this.props.url}/>
+
+            <div> Error 599 </div>
+            </div>
+        )
+    }
+}
+
+
+export default Image;
